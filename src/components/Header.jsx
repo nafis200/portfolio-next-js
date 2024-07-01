d8c4ea1f223b5e50ba95c223e554c842e6cb8ddc
Nafis Ahamed <nafisahamed14@gmail.com>
2024-07-02 05:36:17 +0600

    portfolio done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,34 @@
 import About from "./About";
 import Banner from "./Banner";
 import { Element } from "react-scroll";
-import Navbar from "./Navbar";
 import Empty from "./Empty";
+import Contact from "./Contact";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import Education from "./Education";
 const Header = () => {
     return (
         <div className="">
-            <Empty></Empty>
             <Element name="">
-             <Banner></Banner>
+            <Empty></Empty>
             </Element>
             <Element name="Aboutme">
+             <Banner></Banner>
+            </Element>
+            <Element name="Contacts">
             <About></About>
+            </Element>
+            <Element name="Skills">
+                 <Contact></Contact>
+            </Element>
+            <Element name="">
+                <Skills></Skills>
+            </Element>
+            <Element name="Projects">
+                <Projects></Projects>
+            </Element>
+            <Element name="Education">
+               <Education></Education>
             </Element>
         </div>
     );
